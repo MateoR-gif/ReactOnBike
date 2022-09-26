d82bf416af27ac30ec56e8de94b9b43b5e1d34d4
MateoR-gif <mateorendon008@gmail.com>
2022-09-25 20:07:23 -0500

    Fetch Estaciones Funcionando

--- src/components/listado-estaciones.js
+++ src/components/listado-estaciones.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Card } from 'react-bootstrap'
 import Loading from './loading'
 
-function ListadoEstaciones() {
+
+function ListadoEstaciones({ data }) {
     const [isLoading, setIsLoading] = useState(true)
+    const [stations, setStations] = useState([])
+
+    useEffect(() => {
+        if (data.length === 0) {
+            setIsLoading(true)
+        } else {
+            setStations(data.stations)
+            console.log(stations)
+            setIsLoading(false)
+        }
+    }, [data.length, data.stations, stations])
+
 
     if (isLoading) {
         return (
@@ -12,7 +26,24 @@ function ListadoEstaciones() {
         )
     }
     return (
-        <div className='listado__estaciones'>listado-estaciones</div>
+        <div className='listado__estaciones'>
+            <center>
+                {stations.map(station => {
+                    return (
+                        <div className='listado' key={station.id}>
+                            <Card>
+                                <Card.Title>{station.name}</Card.Title>
+                                <Card.Text>Bicicicletas Libres: {station.free_bikes}</Card.Text>
+                                <Card.Text>Espacios Libres: {station.empty_slots}</Card.Text>
+                                <Card.Text>Última Actualización: {station.timestamp}</Card.Text>
+                                <Card.Text></Card.Text>
+                            </Card>
+                            <br></br>
+                        </div>
+                    )
+                })}
+            </center>
+        </div>
     )
 }
 
--- src/components/resumen-empresas.js
+++ src/components/resumen-empresas.js
@@ -4,10 +4,13 @@ import AccordionBody from "react-bootstrap/esm/AccordionBody"
 import AccordionHeader from "react-bootstrap/esm/AccordionHeader"
 import AccordionItem from "react-bootstrap/esm/AccordionItem"
 import Loading from "./loading"
+import ListadoEstaciones from "./listado-estaciones"
+import './App.css'
 
 function ResumenEmpresas() {
 
     const [networks, setNetworks] = useState([])
+    const [networkInfo, setNetworkInfo] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     const url = "http://api.citybik.es/v2/networks"
 
@@ -26,6 +29,16 @@ function ResumenEmpresas() {
         console.log(response.networks)
     }
 
+    const fetchData2 = async (href) => {
+        let url2 = `http://api.citybik.es${href}`
+        const response = await fetch(url2, { method: 'GET' })
+            .then(response => response.json())
+            .catch(error => console.log(error))
+        console.log(href)
+        setNetworkInfo(response.network)
+        console.log(response.network)
+    }
+
     if (isLoading) {
         return (
             <div>
@@ -34,33 +47,33 @@ function ResumenEmpresas() {
         )
     }
     return (
-        <div className="resumen__empresas">
-            {networks.map(network => {
-                return (
-                    <div key={network.id}>
-                        <Accordion className="accordion" defaultActiveKey="0">
-                            <center>
-                                <AccordionItem>
-                                    <AccordionHeader>{network.company} ({network.location.city})</AccordionHeader>
-                                    <AccordionBody>
-                                        <Card>
-                                            <Card.Title>Información</Card.Title>
-                                            <Card.Text>
-                                                <p>Nombre de la Red: {network.name}</p>
-                                                <p>Nombre de la Empresa: {network.company}</p>
-                                                <p>País: {network.location.country}</p>
-                                            </Card.Text>
-                                        </Card>
-                                        <br></br>
-                                        <Button variant="primary">Mostrar Estaciones</Button>
-                                    </AccordionBody>
-                                </AccordionItem>
-                            </center>
-                        </Accordion>
-                        {/* <p>Nombre de la Compañía: {network.company} ({network.location.city})</p> */}
-                    </div>
-                )
-            })}
+        <div className="content">
+            <ListadoEstaciones data={networkInfo} />
+            <div className="resumen__empresas">
+                {networks.map(network => {
+                    return (
+                        <div key={network.id}>
+                            <Accordion className="accordion" defaultActiveKey="0">
+                                <center>
+                                    <AccordionItem className="accordion__item">
+                                        <AccordionHeader>{network.company} ({network.location.city})</AccordionHeader>
+                                        <AccordionBody>
+                                            <Card>
+                                                <Card.Title>Información</Card.Title>
+                                                <Card.Text>Nombre de la Red: {network.name}</Card.Text>
+                                                <Card.Text>Nombre de la Empresa: {network.company}</Card.Text>
+                                                <Card.Text>País: {network.location.country}</Card.Text>
+                                            </Card>
+                                            <br></br>
+                                            <Button onClick={() => fetchData2(network.href)}>Mostrar Estaciones</Button>
+                                        </AccordionBody>
+                                    </AccordionItem>
+                                </center>
+                            </Accordion>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
